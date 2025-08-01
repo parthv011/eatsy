cb2aacb5b6c291a160aad07d5e1993e0170bf65f
Garala Hasti <hastigarala2524@gmail.com>
2025-08-01 20:55:55 +0530

    Delete admin directory

--- admin/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset();  // Clear all session variables
-session_destroy(); // Destroy the session
-
-// Redirect to home page
-header("Location: ../index.php");
-exit;
